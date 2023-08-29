9d7aec8adc10e13865fb6050d5db1c135fb69a9d
haodong <haodong@ncamc.com.cn>
2023-08-30 01:14:33 +0800

    测试类的autowired变量一直为null，需要增加两个注解：RunWith和SpringBootTest

--- account-service/src/test/java/org/haodong/account/mapper/AccountMapperTest.java
+++ account-service/src/test/java/org/haodong/account/mapper/AccountMapperTest.java
@@ -1,13 +1,19 @@
 package org.haodong.account.mapper;
 
+import org.haodong.account.AccountApp;
 import org.haodong.dto.AccountDto;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = AccountApp.class)
+@RunWith(SpringRunner.class)
 class AccountMapperTest {
 
     @Autowired
@@ -15,6 +21,7 @@ class AccountMapperTest {
     @Test
     void findAllAccounts() {
         List<AccountDto> allAccounts = accountMapper.findAllAccounts();
+        System.out.println(allAccounts);
     }
 
     @Test
